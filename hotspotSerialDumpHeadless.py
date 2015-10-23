6fa011f1751eb9e0fbe40eefe3f93b14aa0b556b
Corey Brady <cbrady@inquirelearning.com>
2015-10-23 10:50:06 -0500

    working to get hotspot code headless

--- hotspotSerialDumpHeadless.py
+++ hotspotSerialDumpHeadless.py
@@ -9,6 +9,8 @@ DEBUG_SERIAL_LIST = 1
 ### OPTIONS ###
 
 theserver = 'http://127.0.0.1:8000/'
+#theserver = 'http://192.168.1.42:8000/'
+
 
 ### HELPER FUNCTIONS ###
 def serial_ports():
@@ -46,114 +48,121 @@ def stripped(string):
 	return ''.join([i for i in string if 31 < ord(i) < 127])
 ### HELPER FUNCTIONS ###
 
-### MAIN ###
-try:
-	ports = serial_ports()
-	for p in ports:
-		try:
-			# Try opening port, send reset signal and establish connection
-			if HARDCODED_PORT == 1:
-				port = serial.Serial(HARD_PORT_STRING, 115200, timeout=1)
-			else:
-				port = serial.Serial(p, 115200, timeout=1)
-			port.close()
-			break
-		except:
-			# Keep querying until we find a port
-			pass
-	try:
-		port.isOpen()
-	except:
-		raw_input('Unable to find a hotspot, press Enter to exit.')
-		exit()
 
-	# Wait until we receive handshake
-	if HARDCODED_PORT == 1:
-		print 'Hardcoded port opened, connecting to hotspot.'
-	else:
-		print 'Dynamic port opened, connecting to hotspot.'
-	port.open()
-	while stripped(port.readline()) != 'Propeller':
-		time.sleep(1)
-	print 'Connected to hotspot.'
+def mainloop():
+    ### MAIN ###
+    try:
+        ports = serial_ports()
+        for p in ports:
+            try:
+                # Try opening port, send reset signal and establish connection
+                if HARDCODED_PORT == 1:
+                    port = serial.Serial(HARD_PORT_STRING, 115200, timeout=1)
+                else:
+                    port = serial.Serial(p, 115200, timeout=1)
+                port.close()
+                break
+            except:
+                # Keep querying until we find a port
+                pass
+        try:
+            port.isOpen()
+        except:
+            print('Unable to find a hotspot, retrying in 500 ms.')
+            time.sleep(0.5);
+            print("retrying")
+            time.sleep(0.1);
+            return
 
-	# Send handshake to ensure connection
-	print 'Initializing stream.'
-	port.write('H0st\n')
-	time.sleep(1)
-	print 'Opened stream.\n'
+        # Wait until we receive handshake
+        if HARDCODED_PORT == 1:
+            print 'Hardcoded port opened, connecting to hotspot.'
+        else:
+            print 'Dynamic port opened, connecting to hotspot.'
+        port.open()
+        while stripped(port.readline()) != 'Propeller':
+            time.sleep(1)
+        print 'Connected to hotspot.'
 
-	while True:
+        # Send handshake to ensure connection
+        print 'Initializing stream.'
+        port.write('H0st\n')
+        time.sleep(1)
+        print 'Opened stream.\n'
 
-		# Keep an open stream of data
-		while 'txBegin' not in stripped(port.readline()):
-			pass
+        while True:
 
-		# Debug transmission
-		#print port.read(port.inWaiting())
-		#print port.inWaiting()
+            # Keep an open stream of data
+            while 'txBegin' not in stripped(port.readline()):
+                pass
 
-		timeout_buffer = port.readline()
-		if stripped(timeout_buffer) == 'Timeout':
-			print str(datetime.datetime.now()) + ': IR connection timed out, please try again.'
-			continue
+            # Debug transmission
+            #print port.read(port.inWaiting())
+            #print port.inWaiting()
 
-	    # Get records, should always find a record
-		num_records = int(timeout_buffer)
+            timeout_buffer = port.readline()
+            if stripped(timeout_buffer) == 'Timeout':
+                print str(datetime.datetime.now()) + ': IR connection timed out, please try again.'
+                continue
 
-		badge_ids = []
-		badge_times = []
-		badge_itypes = []
+            # Get records, should always find a record
+            num_records = int(timeout_buffer)
 
-		for i in xrange(0, num_records):
-			t = []
-			t = (stripped(port.readline())).split(',')
-			badge_ids.append(t[0])
-			badge_times.append(t[1])
-			badge_itypes.append(t[2])
+            badge_ids = []
+            badge_times = []
+            badge_itypes = []
 
-		badge_timeframe = int(stripped(port.readline()))
+            for i in xrange(0, num_records):
+                t = []
+                t = (stripped(port.readline())).split(',')
+                badge_ids.append(t[0])
+                badge_times.append(t[1])
+                badge_itypes.append(t[2])
 
-		if 'DUMP' not in badge_itypes[0]:
-			print str(datetime.datetime.now()) + ': Retrieved corrupted content, please try again.'
-			continue
+            badge_timeframe = int(stripped(port.readline()))
 
-		print str(datetime.datetime.now()) + ': Retrieved interactions, saving to file.'
-		
-		
-		#dt_now = datetime.datetime.now()
-		#INSTEAD of getting time from local system, get time from server
-		j = urllib2.urlopen(theserver+'time/')
-		j_obj = json.load(j)
-		timestring = j_obj['time']
-		dt_now = datetime.datetime.strptime(timestring, "%Y-%m-%d %H:%M:%S.%f")
+            if 'DUMP' not in badge_itypes[0]:
+                print str(datetime.datetime.now()) + ': Retrieved corrupted content, please try again.'
+                continue
+
+            print str(datetime.datetime.now()) + ': Retrieved interactions, saving to file.'
 
 
-		if DEBUG_SERIAL_LIST == 1:
-			print 'IDs: ' + str(badge_ids)
-			print 'Times: ' + str(badge_times)
-			print 'Types: ' + str(badge_itypes)
+            #dt_now = datetime.datetime.now()
+            #INSTEAD of getting time from local system, get time from server
+            j = urllib2.urlopen(theserver+'time/')
+            j_obj = json.load(j)
+            timestring = j_obj['time']
+            dt_now = datetime.datetime.strptime(timestring, "%Y-%m-%d %H:%M:%S.%f")
 
-		#IN ADDITION to saving to file, also Post to server
-		data = {}
-		data['interactions'] = [];
-		for i in xrange(2, num_records):
-			data['interactions'].append(badge_ids[1] + ',' + badge_ids[i] + ',' + (dt_now - datetime.timedelta(seconds=badge_timeframe) + datetime.timedelta(seconds=int(badge_times[i]))).strftime('%H:%M:%S'))
-		#print(data);
-		req = urllib2.Request(theserver+'addmany/')
-		req.add_header('Content-Type', 'application/json')
-		response = urllib2.urlopen(req, json.dumps(data))
-		
-		
-		# Dump to file
-		f = open('hotspotdata.txt', 'a')
-		for i in xrange(2, num_records):
-			f.write(badge_ids[1] + ',' + badge_ids[i] + ',' + (dt_now - datetime.timedelta(seconds=badge_timeframe) + datetime.timedelta(seconds=int(badge_times[i]))).strftime('%H:%M:%S')  + '\n')
-		f.close()
 
-except Exception, err:
-	print(traceback.format_exc())
-	#raw_input('Enter to continue.')
-	port.close()
+            if DEBUG_SERIAL_LIST == 1:
+                print 'IDs: ' + str(badge_ids)
+                print 'Times: ' + str(badge_times)
+                print 'Types: ' + str(badge_itypes)
 
-# checksum on content (pass number to serial and then checksum on the host)
+            #IN ADDITION to saving to file, also Post to server
+            data = {}
+            data['interactions'] = [];
+            for i in xrange(2, num_records):
+                data['interactions'].append(badge_ids[1] + ',' + badge_ids[i] + ',' + (dt_now - datetime.timedelta(seconds=badge_timeframe) + datetime.timedelta(seconds=int(badge_times[i]))).strftime('%H:%M:%S'))
+            print(data);
+            req = urllib2.Request(theserver+'addmany/')
+            req.add_header('Content-Type', 'application/json')
+            response = urllib2.urlopen(req, json.dumps(data))
+
+
+            # Dump to file
+            f = open('hotspotdata.txt', 'a')
+            for i in xrange(2, num_records):
+                f.write(badge_ids[1] + ',' + badge_ids[i] + ',' + (dt_now - datetime.timedelta(seconds=badge_timeframe) + datetime.timedelta(seconds=int(badge_times[i]))).strftime('%H:%M:%S')  + '\n')
+            f.close()
+
+    except Exception, err:
+        print(traceback.format_exc())
+        #raw_input('Enter to continue.')
+        port.close()
+
+    # checksum on content (pass number to serial and then checksum on the host)
+
+mainloop()
